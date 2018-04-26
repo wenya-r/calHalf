54bbfa1caa68e593753482a736454669fbfb7f52
wenya-r <wwrowe@gmail.com>
2018-04-26 08:50:56 +0000

    update file

--- calhalf.cpp
+++ calhalf.cpp
@@ -8,6 +8,7 @@
 #include <functional>
 #include <ctime>
 #include <atomic>
+#include <chrono>
 
 using namespace std;
 
@@ -20,38 +21,59 @@ void half(int num, int size)
     uniform_int_distribution<int> dist(0,1);
 
     auto gen = std::bind(dist, eng);
+    
+    this_thread::sleep_for(chrono::milliseconds(10000));
 
     vector<int> vec(size);
     generate(begin(vec), end(vec), gen);
     float average = accumulate( vec.begin(), vec.end(), 0.0)/vec.size();      
     cout << average << " has value " << endl;
-
+//    return average;
 }
 
 
 int main(int nNumberofArgs, char* pszArgs[]){
 unsigned int nThreads = thread::hardware_concurrency();
 cout << "number of threads " << nThreads << endl;
-float curtime = clock();
+auto curtime = chrono::high_resolution_clock::now();
 
 
-vector<thread> threads
-for (int i=0; i<10; ++i) {
-    threads.push_back(thread{ half, {i, 1000000}});
-}
-for (auto& t : threads){
-    t.join();
-}
-//thread t1(half, 1, 1000000);
-//thread t2(half, 2, 1000000);
-//thread t3(half, 3, 1000000);
-//thread t4(half, 4, 1000000);
-//t1.join();
-//t2.join();
-//t3.join();
-//t4.join();
+//vector<thread> threads
+//for (int i=0; i<10; ++i) {
+  //  threads.push_back(thread{ half, {i, 10000000}});
+//}
+//for (auto& t : threads){
+  //  t.join();
+//}
+int turns = 800000000;
 
-cout << "Time :"<< (clock() - curtime)/CLOCKS_PER_SEC << endl;
+thread t1(half, 1, turns);
+thread t2(half, 2, turns);
+thread t3(half, 3, turns);
+thread t4(half, 4, turns);
+
+thread t5(half, 5, turns);
+thread t6(half, 6, turns);
+thread t7(half, 7, turns);
+thread t8(half, 8, turns);
+thread t9(half, 9, turns);
+
+
+t1.join();
+t2.join();
+t3.join();
+t4.join();
+t5.join();
+t6.join();
+t7.join();
+t8.join();
+t9.join();
+
+
+chrono::duration<double, milli> elapsed = chrono::high_resolution_clock::now() - curtime;
+
+cout << "Time :"<< elapsed.count() << endl;
 return 0;
 }
+
 
